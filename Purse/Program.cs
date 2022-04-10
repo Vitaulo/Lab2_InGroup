47dbcd05899ad0e7a70c05c7b7dc8137daae5ff5
Vitaulo <gusvital123@gmail.com>
2022-04-11 00:55:12 +0300

    add Purse

---
+++ Purse/Program.cs
@@ -0,0 +1,14 @@
+﻿//Console.WriteLine("Hello, World!");
+
+namespace Purse
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Purse");
+        }
+    }
+}
+
